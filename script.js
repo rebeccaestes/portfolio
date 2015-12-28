1c8d3047d94d5e35dfe7008c4552b15f526b5421
Rebecca Estes <estes.rebecca@gmail.com>
2015-12-28 15:43:40 -0500

    projects section work

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ $(document).ready(function(){
 
 	console.log("jQuery loaded :D");
 
+	$("figure").hover (function() {
+		$(this).find(".moreinfo").fadeIn();
+		$(this).find("figcaption").css("height", "75px").css("line-height", "12px").css("padding-top", "5px");
+
+	}, function() {
+		$(this).find("figcaption").css("height", "25px").css("line-height", "25px").css("padding-top", "0px");
+		$(this).find(".moreinfo").fadeOut();
+	});
+	
 
 });
