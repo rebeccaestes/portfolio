3eade1ac63a476b0961ac692491ec833a5d76f09
Rebecca Estes <estes.rebecca@gmail.com>
2015-12-28 23:09:51 -0500

    publishable

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ $(document).ready(function(){
 	console.log("jQuery loaded :D");
 
 	$("figure").hover (function() {
-		$(this).find(".moreinfo").fadeIn();
-		$(this).find("figcaption").css("height", "75px").css("line-height", "12px").css("padding-top", "5px");
+		$(this).find(".moreinfo").fadeIn("slow");
+		$(this).find("figcaption").css("height", "85px").css("line-height", "12px").css("padding-top", "5px");
 
 	}, function() {
 		$(this).find("figcaption").css("height", "25px").css("line-height", "25px").css("padding-top", "0px");
 		$(this).find(".moreinfo").fadeOut();
 	});
 	
+	// $(window).scroll(function() {
+	// 	if ($(document).scrollTop() > 5) {
+	// 		$("#contact").fadeIn();
+	// 	}
+	// 	else {
+	// 		$("#contact").fadeOut();
+	// 	}
+	// })
 
 });
