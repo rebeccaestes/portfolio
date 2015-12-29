a9d051b8ce9406305449c6ea7bfb3a2e157a604c
Rebecca Estes <estes.rebecca@gmail.com>
2015-12-29 00:16:36 -0500

    done for tonight

--- script.js
+++ script.js
@@ -4,19 +4,30 @@ $(document).ready(function(){
 
 	$("figure").hover (function() {
 		$(this).find(".moreinfo").fadeIn("slow");
-		$(this).find("figcaption").css("height", "85px").css("line-height", "12px").css("padding-top", "5px");
+		$(this).find("figcaption").css("height", "85px")
+															.css("line-height", "12px")
+															.css("padding-top", "5px");
 
 	}, function() {
-		$(this).find("figcaption").css("height", "25px").css("line-height", "25px").css("padding-top", "0px");
+		$(this).find("figcaption").css("height", "25px")
+															.css("line-height", "25px")
+															.css("padding-top", "0px");
 		$(this).find(".moreinfo").fadeOut();
 	});
 	
 	// $(window).scroll(function() {
-	// 	if ($(document).scrollTop() > 5) {
-	// 		$("#contact").fadeIn();
-	// 	}
-	// 	else {
-	// 		$("#contact").fadeOut();
+	// 	if ($(document).scrollTop() < 600) {
+	// 		$("#contact").css("height", "50px")
+	// 								.css("text-align", "center")
+	// 								.css("top", "88%")
+	// 								.css("width", "100%")
+	// 								.css("margin", "0 auto")
+	// 								.css("float", "none");
+	// 								.css("position", "static")
+	// 		$(".contact").css("display", "inline-block")
+	// 								.css("float", "left")
+	// 								.css("text-align", "center");
+
 	// 	}
 	// })
 
