e3da560279554170f5e23630be996b379d79151c
Rebecca Estes <estes.rebecca@gmail.com>
2016-01-02 13:57:16 -0500

    much changes

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
 
-	console.log("jQuery loaded :D");
 
-	$("figure").hover (function() {
-		$(this).find(".moreinfo").fadeIn("slow");
-		$(this).find("figcaption").css("height", "85px")
-															.css("line-height", "12px")
-															.css("padding-top", "5px");
+	// $("figure").hover (function() {
+	// 	$(this).find(".moreinfo").fadeIn("slow");
+	// 	$(this).find("figcaption").css("height", "85px")
+	// 														.css("line-height", "12px")
+	// 														.css("padding-top", "5px");
 
-	}, function() {
-		$(this).find("figcaption").css("height", "25px")
-															.css("line-height", "25px")
-															.css("padding-top", "0px");
-		$(this).find(".moreinfo").fadeOut();
-	});
+	// }, function() {
+	// 	$(this).find("figcaption").css("height", "25px")
+	// 														.css("line-height", "25px")
+	// 														.css("padding-top", "0px");
+	// 	$(this).find(".moreinfo").fadeOut();
+	// });
 	
 	// $(window).scroll(function() {
 	// 	if ($(document).scrollTop() < 600) {
