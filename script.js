3fe2b332e86114b6b5e8665e40b2e2e86fa285b4
Rebecca Estes <estes.rebecca@gmail.com>
2016-03-02 22:23:58 -0500

    added link to kitten photo

--- script.js
+++ script.js
@@ -10,4 +10,19 @@ $(document).ready(function(){
 		$(e.target).closest(".project_about").find(".more_about").fadeOut("slow");
 	})
 
+	$(".kitten-link").click(function() {
+		$(".container").each(function() {
+			$(this).css("opacity", ".5");
+		});
+		$(".kitten").fadeIn("slow");
+
+	})
+
+	$(".kitten-close").click(function() {
+		$(".kitten").fadeOut("slow");
+		$(".container").each(function() {
+			$(this).fadeTo("slow", 1)
+		}) 
+	})
+
 });
