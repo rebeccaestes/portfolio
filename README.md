7cb89022a97e321478bf5e60a265c761b99bf495
Rebecca Estes <estes.rebecca@gmail.com>
2016-03-13 13:07:17 -0400

    new & completely improved

--- README.md
+++
@@ -1,3 +0,0 @@
-# Hire me :)
-
-www.rebeccaestes.com
--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
 
+	// $(".readysetom").on("click", function() {
+	// 	var rso_skills = $(".skills").find($(".rso"));
+	// 	console.log(rso_skills);
+	// 	rso_skills.addClass("skills-used");
+	// })
+
 	$(".more").click(function(e) {
 		$(e.target).closest(".project_about").find(".more_about").fadeIn("slow");
-		$(e.target).fadeOut("slow");
+		// $(e.target).fadeOut("slow");
+		// $(e.target).css("color", "white");
+		$(e.target).hide();
+		$(e.target).next().show();
 	})
 
 	$(".less").click(function(e) {
 		$(e.target).closest(".project_about").find(".more").fadeIn("slow");
 		$(e.target).closest(".project_about").find(".more_about").fadeOut("slow");
+		$(e.target).hide();
+		$(e.target).previous.show()
+
 	})
 
 	$(".kitten-link").click(function() {
@@ -15,7 +27,6 @@ $(document).ready(function(){
 			$(this).css("opacity", ".5");
 		});
 		$(".kitten").fadeIn("slow");
-
 	})
 
 	$(".kitten-close").click(function() {
